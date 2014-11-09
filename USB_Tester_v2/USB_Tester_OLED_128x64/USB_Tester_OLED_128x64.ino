f947a69426f6b9c9ea1c1947c5f518e3adb20ee3
William <william@friedcircuits.us>
2014-11-09 13:37:49 -0800

    See changelog
    Cleaned up screens, added bar graphs and firmware version command. Using
    ug8lib.

--- USB_Tester_v2/USB_Tester_OLED_128x64/USB_Tester_OLED_128x64.ino
+++ USB_Tester_v2/USB_Tester_OLED_128x64/USB_Tester_OLED_128x64.ino
@@ -67,22 +67,30 @@ so action happens after button is let go
 2014.01.12 - William Garrido
 - Improve D-\+ Analog readings by using internal 1.1v ref and dividing by 1023 instead of 1024
 - Split energy readings to multiple lines to take use of larger display and give room for larger readers
+
+2014-10-10 - William Garrido
+- Switch to UG8Lib for graphics handling
+- Split update scope function, only updates graph. Graph data updates is outside picture loop.
+- Spilt message handling so that variables are updated outside picture loop.
+- Clean up code
+- Added command to get firmware version
 **************************************/
 
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_INA219.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <U8glib.h>
 #include <TimerOne.h>
 #include <ClickButton.h>
 
+#define FW_VERSION 2.10
+
 // All hardware pin usage is defined here:
-#define OLED_DC 5
-#define OLED_CS SS
-#define OLED_CLK SCK
-#define OLED_MOSI MOSI
-#define OLED_RESET 9
+//#define OLED_DC 5
+//#define OLED_CS SS
+//#define OLED_CLK SCK
+//#define OLED_MOSI MOSI
+//#define OLED_RESET 9
 #define LEDPIN 13
 #define BTN_PIN 10
 //USB Data Lines
@@ -97,10 +105,9 @@ so action happens after button is let go
 
 #define DEBUG 0
 
-uint16_t ledWarn = 350; //Threshold in mA
+uint16_t ledWarn = 400; //Threshold in mA
 
 // Graph values:
-
 // Graph area is from 0 to 127
 #define GRAPH_MEMORY 128
 // We store the values as unsigned 16 bit integers (0 to 65535), rather than floats,
@@ -139,10 +146,11 @@ uint8_t graphX = 0; //Start of X for graph
 uint8_t graphY = 0;  //y placement of graph
 
 //Init OLED Display
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
-
+//Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+U8GLIB_SSD1306_128X64 display(SS, 5, 9);
   
 //FriedCircuits Logo for startup 
+/*
 static unsigned char FriedCircuitsUSBTester[] PROGMEM = 
 {
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0F, 0x0F, 0x87, 0x87, 0xC0, 0x0C, 0x41, 0x86, 0x00,
@@ -170,7 +178,7 @@ static unsigned char FriedCircuitsUSBTester[] PROGMEM =
 0x03, 0x7F, 0x7E, 0x19, 0x99, 0xFF, 0x80, 0xFB, 0xB9, 0x8F, 0x3C, 0x39, 0xC6, 0x18, 0xF1, 0xC0,
 0x03, 0xFC, 0x00, 0x00, 0x00, 0x7F, 0x07, 0xE0, 0x00, 0x00, 0x1C, 0x0C, 0x0C, 0x40, 0x18, 0x30
 };
-
+*/
 // Voltages are now read in the interrupt routine, and available in
 // global (volatile because modified within an interrupt) variables:
 volatile float shuntvoltage = 0;
@@ -218,6 +226,8 @@ uint8_t oldScreen = 0;
 bool msgDisplay = false;
 #define MSGSCREEN 6;
 
+//Track message display time, made global instead of static so that it is not updated during picture loop
+uint8_t msgTime = 0;
 
 /**
  * Initialisation of the sketch
@@ -241,28 +251,27 @@ void setup()
   ina219.begin();
   
   //by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
-  display.begin(SSD1306_SWITCHCAPVCC);
+  //display.begin(SSD1306_SWITCHCAPVCC);
   
   //Setup display
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
+  //display.setTextSize(1);
+  display.setFont(u8g_font_6x12);
+  //display.setTextColor(WHITE);
+  display.setColorIndex(1);
  
   // Initialize ring buffer
   for (uint8_t i=0; i < GRAPH_MEMORY; i++) {
     graph_Mem[i] = 0;
   }
-
-  
-  //show splashscreen
-  display.clearDisplay(); 
-  display.drawBitmap(0, 0, FriedCircuitsUSBTester, 128, 24, WHITE);
-  display.display();
+/*
+  display.firstPage();
+  do {
+   display.drawStr( 2, 10, "USB Tester 2.0");
+   display.drawStr( 5, 20, "FriedCircuits.us");
+  }while( display.nextPage());
   delay(1000);
-  display.clearDisplay();   // clears the screen and buffer
-  
   digitalWrite(LEDPIN, LOW);
-  
-  
+   */
   Timer1.initialize(READFREQ); // 100ms reading interval
   Timer1.attachInterrupt(readADCs); 
 }
@@ -330,7 +339,7 @@ void readADCs() {
  */
 void loop()
 {
-  
+  //display.firstPage();
   modeBtn.Update();  
   unsigned long now = millis();
 
@@ -343,14 +352,22 @@ void loop()
     dpVoltage = analogRead(USB_DP) * vcc / 1023;
     dmVoltage = analogRead(USB_DM) * vcc / 1023;
 
-    
-    // For some reason the display offset jumps around every once in a while
-    // when opening the USB serial port. This sounds like a big bug, but I was not
-    // able to track it down (E. Lafargue)
-    display.ssd1306_command(SSD1306_SETDISPLAYOFFSET);              // 0xD3
-    display.ssd1306_command(0x0);                                   // no offset
+   //Refresh graph from current sensor data
+    drawGraph(current_mA);
+    //update msg outside picture loop before next display refresh
+    if(current_screen == 6){
+      if (msgTime <= setDisplayTime){
+      msgTime++;
+      }
+      else {
+        current_screen = oldScreen;
+        msgTime = 0;
+        msgDisplay=false;
+      }
+    }
 
-    display.clearDisplay();
+    display.firstPage();
+	do{
     switch (current_screen) {
       case 0:
         drawScope();
@@ -378,7 +395,7 @@ void loop()
     }
     
     drawBottomLine();
-    display.display();
+	} while (display.nextPage() );
     lastDisplay = now;
   }
 
@@ -408,7 +425,7 @@ void loop()
       input_Buffer_Index = (input_Buffer_Index+1)%INPUT_BUFFER_SIZE;
   }
 
-  uint16_t btnState = digitalRead(BTN_PIN);
+  uint8_t btnState = digitalRead(BTN_PIN);
   
   if ((current_mA >= ledWarn) || (btnState)){
     digitalWrite(LEDPIN, HIGH);
@@ -434,11 +451,11 @@ void processInput() {
     case 'R':
       serialOutputRate = atoi(&input_Buffer[2]);
       if (serialOutputRate < 150) serialOutputRate = 150; // We sample at 100ms, so we need to remain above that value
-      Serial.print(F("{ \"R\":")); Serial.print(serialOutputRate);Serial.println("}");
+      Serial.print(F("{ \"R\":")); Serial.print(serialOutputRate);Serial.println(F("}"));
       break;
     case 'S':
        current_screen = (input_Buffer[2]-49) % MAX_SCREENS;
-       Serial.print(F("{\"S\":"));Serial.println(current_screen);Serial.println("}");
+       Serial.print(F("{\"S\":"));Serial.print(current_screen);Serial.println(F("}"));
        break;
     case 'Z':
        setButtonMode(-1);
@@ -447,81 +464,89 @@ void processInput() {
     case 'W':
        ledWarn = atoi(&input_Buffer[2]);
        if (ledWarn > 2000) ledWarn = 2000;
-       Serial.print(F("{\"W\":")); Serial.println(ledWarn);Serial.println("}");
+       Serial.print(F("{\"W\":")); Serial.print(ledWarn);Serial.println(F("}"));
        break;
+    case 'V':
+       Serial.print(F("{\"V\":")); Serial.print(FW_VERSION);Serial.println(F("}"));
     default:
       break;
   }
 }
 
 
-
 void drawBottomLine() {
   //Set x,y and print sensor data
-  display.setCursor(0,55);
+  display.setPrintPos(0,64);
   display.print(loadvoltage);   display.print(F("V "));
   printJustified(current_mA);   display.print(F("mA "));
-  display.print((current_mA*loadvoltage)/1000);  display.print("W");  
-
+  //display.print((current_mA*loadvoltage)/1000);  display.print("W");  
+  printJustified2((current_mA*loadvoltage)/1000, 2); display.print("W"); 
 }
-
 
 // Our various screens:
 void drawScope() {
-    //Refresh graph from current sensor data
-  drawGraph(current_mA);
+
+  for (uint8_t i=0; i < GRAPH_MEMORY; i++) {
+    uint8_t val = 54 - map(graph_Mem[(i+ring_idx)%GRAPH_MEMORY], 0, autoscale_limits[graph_MAX], 0, 54);
+    display.drawPixel(i, val);
+  }
   
   // Display current scale
   // Note: commented out, not quite sure this is very important since
   // the graph is merely a trend indicator - we only have 24 pixels after
   // all
-  display.setCursor(104,0);
+  display.setPrintPos(104,7);
   display.print((autoscale_limits[graph_MAX]+0.0)/1000);
-
-   
 }
 
 // Draw current energy values and peak
 // power
 void drawEnergy() {
-  display.setCursor(0,0);
+  display.setPrintPos(28,7);
+  display.print(F("Energy Usage"));
+  display.drawHLine(0,7,128);
+  display.setPrintPos(0,17);
   printJustified(milliwatthours,2);
-  display.println(F("mWh "));
+  display.print(F("mWh "));
+  display.setPrintPos(64,17);
   printJustified(milliamphours,2);
   display.print(F("mAh"));
 
-  display.setCursor(0,32);
+  display.setPrintPos(0,32);
   display.print(F("Peak: "));
   display.print(voltageAtPeakPower*currentAtPeakPower/1000);
   display.print("W");
-  display.setCursor(0,40);
+  display.setPrintPos(0,40);
   display.print(F("@ "));
   display.print(voltageAtPeakPower);
   display.print(F("V & "));
   display.print(currentAtPeakPower);
   display.print(F("mA"));
   
-  display.drawFastHLine(0,53,128,WHITE);
+  display.drawHLine(0,53,128);
 
 }
 
 
 // Displays peak and minimum values
 void drawPeakMins() {
-  display.setCursor(0,0);
+  display.setPrintPos(28,7);
+  display.print(F("Peak - Mins"));
+  display.drawHLine(0,7,128);
+  display.setPrintPos(0,17);
   display.print(F("Peak:"));
   printJustified(peakCurrent);
   display.print(F("mA ("));
   display.print(voltageAtPeakCurrent);
   display.print(F("V) "));
-  display.setCursor(0,9);
+  display.setPrintPos(0,32);
   display.print(F("Min:"));
   display.print(minVoltage);
   display.print(F("V ("));
   display.print(currentAtMinVoltage);
   display.print(F("mA)"));
   
-  display.drawFastHLine(0,53,128,WHITE);
+  display.drawHLine(0,53,128);
 
 }
 
@@ -529,21 +554,85 @@ void drawPeakMins() {
 // Display width is 7 digits wide, we leave the 2 rightmost digits
 // for unit display
 void drawBig(float val, char* unit, uint8_t decimals) {
-  display.setCursor(0,0);
-  display.setTextSize(3);
+  display.setPrintPos(50,16);
+  display.setFont(u8g_font_10x20);
   if ((decimals < 2) && (val < 1000)) display.print(" ");
   if ((decimals < 1) && (val < 10000)) display.print(" ");
   if (val < 100) display.print(" ");
   if (val < 10) display.print(" ");
   display.print(val, decimals);
   display.print(unit);
-  display.setTextSize(1);
-  display.println();
-  display.print(F("D+: "));
-  display.println(dpVoltage);
-  display.print(F("D-: "));
-  display.print(dmVoltage);
+  display.setFont(u8g_font_6x12);
   
+  
+  display.drawRFrame(0,24,128,20,1);
+  float limit = 0.0;
+  if(unit == "W"){
+    display.setPrintPos(0,7);
+    display.print(F("Peak:"));
+    display.setPrintPos(0,16);
+    display.print(voltageAtPeakPower*currentAtPeakPower/1000);
+    display.print("W");
+    
+    float wattMax = 1.10;
+    if(val > 10.10){limit = 0.201; wattMax = 25.10;}
+    else if (val > 1.10){limit = 0.041; wattMax = 5.10;}
+    else{limit = 0.009;} //1W range
+    for(int x = 2; x <= 126; x++){
+      if ((float)x <= ((float)val/limit)){display.drawVLine(x,26,16);}  
+    }
+    //display.drawVLine((voltageAtPeakCurrent/limit),26,16);
+    display.drawVLine(0,44,8); 
+    display.setPrintPos(1,52); display.print(F("0"));
+    display.drawVLine(64,44,8);
+    display.setPrintPos(32,52); display.print(wattMax/2);  
+    display.setPrintPos(97,52); display.print(wattMax);
+    display.drawVLine(127,44,8); 
+  }
+ 
+  
+  if(unit == "mA"){
+    display.setPrintPos(0,7);
+    display.print(F("Peak:"));
+    display.setPrintPos(0,16);
+    display.print(peakCurrent);
+    display.print("mA");
+
+    limit = (float)autoscale_limits[graph_MAX]/127; 
+    for(int x = 2; x < 126; x++){
+      if (x <= (val/limit)){display.drawVLine(x,26,16);}  
+    }
+    display.drawVLine((peakCurrent/limit),26,16);
+    display.drawVLine(0,44,8); 
+    display.setPrintPos(1,52); display.print(F("0"));
+    display.drawVLine(64,44,8); 
+    display.setPrintPos(32,52); display.print(((autoscale_limits[graph_MAX]+0.0)/1000)/2); display.print(F("A"));
+    display.setPrintPos(97,52); display.print((autoscale_limits[graph_MAX]+0.0)/1000); display.print(F("A"));
+    display.drawVLine(127,44,8); 
+  }
+  if(unit == "V"){
+    display.setPrintPos(0,7);
+    display.print(F("D+: "));
+    display.print(dpVoltage);
+    display.setPrintPos(0,16);
+    display.print(F("D-: "));
+    display.print(dmVoltage);
+  
+    float voltMax = 5.10;
+    if(val > 12.10){limit = 0.21; voltMax = 26.0;}
+    else if (val > 5.10){limit = 0.098; voltMax = 12.10;}
+    else{limit = 0.041;} //5V range
+    for(int x = 2; x <= 126; x++){
+      if ((float)x <= ((float)val/limit)){display.drawVLine(x,26,16);}  
+    }
+    //display.drawVLine((voltageAtPeakCurrent/limit),26,16);
+    display.drawVLine(0,44,8); 
+    display.setPrintPos(1,52); display.print(F("0"));
+    display.drawVLine(64,44,8);
+    display.setPrintPos(32,52); display.print(voltMax/2);  
+    display.setPrintPos(97,52); display.print(voltMax);
+    display.drawVLine(127,44,8); 
+  }
 }
 
 //Displays message in large font on entire display
@@ -559,19 +648,11 @@ void setMsg(char* msg, uint16_t time)
 }
 void drawMsg()
 {
-  static unsigned msgTime = 0;
-  
   if (msgTime <= setDisplayTime){
-  display.setCursor(0,0);
-  display.setTextSize(4);
+  display.setPrintPos(0,16);
+  display.setFont(u8g_font_10x20);
   display.print(setMsgDisplay);
-  display.setTextSize(1);
-  msgTime++;
-  }
-  else {
-    current_screen = oldScreen;
-    msgTime = 0;
-    msgDisplay=false;
+  display.setFont(u8g_font_6x12);
   }
   
 }
@@ -579,13 +660,11 @@ void drawMsg()
 // Draw the complete graph. This is also where we update the ring
 // buffer for graph values
 void drawGraph(float reading) {
-  
   // Adjust scale: we have GRAPH_MEMORY points, so whenever
   // we cross a scale boundary, we initiate a countdown timer.
   // This timer goes down at each redraw if we're under the previous lower scale
   // boundary, otherwise it is reset. If we stay below the scale boundary until it
   // reaches zero, then we scale down.
-
 
   if (reading > autoscale_limits[graph_MAX]) {
     // We need to scale up:
@@ -597,9 +676,9 @@ void drawGraph(float reading) {
     autoscale_countdown = GRAPH_MEMORY;
     autoscale_max_reading = 0;
     // Let user know the values just got rescaled
-    display.setCursor(122,0);
-    display.print("*");
-    delay(100);
+    //display.setPrintPos(122,7);
+    //display.print("*");
+    //delay(100);
   } else if (graph_MAX > 0) {
     // Do we need to scale down ?
     if (reading < autoscale_limits[graph_MAX-1]) {
@@ -619,24 +698,18 @@ void drawGraph(float reading) {
         autoscale_countdown = GRAPH_MEMORY;
         autoscale_max_reading = 0;
         // Let user know the values just got rescaled
-        display.setCursor(122,0);
-        display.print("*");
-        delay(100);
+        //display.setPrintPos(122,7);
+        //display.print("*");
+        //delay(100);
       }
     } else {
       autoscale_countdown = GRAPH_MEMORY; // we are above the scale under us
       autoscale_max_reading = 0;
     }
   }
-  
   graph_Mem[ring_idx] = (uint16_t) reading;
   ring_idx = (ring_idx+1)%GRAPH_MEMORY;
-  for (uint8_t i=0; i < GRAPH_MEMORY; i++) {
-    uint8_t val = 54 - map(graph_Mem[(i+ring_idx)%GRAPH_MEMORY], 0, autoscale_limits[graph_MAX], 0, 54);
-    display.drawPixel(i, val , WHITE);
-  }
 }
-
 
   //Serial output for data logging with Java app
 void serialOutput() {
@@ -676,16 +749,21 @@ void printJustified(float val, uint8_t dec)
   if (val < 1000) display.print(" ");
   if (val < 100) display.print(" ");
   if (val < 10) display.print(" ");
-  
   display.print(val,dec); 
-
 }
 
 void printJustified(float val) {
   printJustified(val,0);
-  
 }
 
+void printJustified2(float val, uint8_t dec)
+{
+  //val = floor(val + 0.5);
+  if (val < 1000) display.print(" ");
+  if (val < 100) display.print(" ");
+  if (val < 10) display.print(" ");
+  display.print(val,dec); 
+}
 
 //Copy of Arduino map function converted for floats, from a forum post
 float mapf(float x, float in_min, float in_max, float out_min, float out_max)
@@ -703,9 +781,7 @@ int freeRam () {
 //Handles setting screen and reseting points\
 //Can be used to add function based on 2+ clicks
 void setButtonMode(int8_t btnClicks){
-
   switch (btnClicks){
-    
     case -1: //longpress
         setMsg("RESET", 10);
         peakCurrent = 0;
@@ -717,18 +793,13 @@ void setButtonMode(int8_t btnClicks){
         milliwatthours = 0;
         milliamphours = 0;
         break;
-    
     case 1:
         current_screen = (current_screen + 1) % MAX_SCREENS;
     break;
-    
    //default:
    
   }
-    
 }
-
-
 
 long readVcc() {
   //http://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
---
+++ changelog.md
@@ -0,0 +1,23 @@
+FC-USB-Tester-OLED-Backpack
+===========================
+v2.10
+-Start of changelog
+-Switch to UG8Lib display library
+-Clean up Energy and Peak\Mins screens
+-Updated large font screens with bar graphs that auto scale
+-Added firmware version with serial command
+
+Note: Java app v1.0+ can check for new versions of the firmware from github
+ 
+
+
+
+License: All source code and designs are released under 
+
+Creative Commons NonCommercial-ShareAlike 
+
+CC BY-NC-SA
+
+![CC BY-NC-SA](http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png)
+
+Please contact me for commercial opportunities. 
